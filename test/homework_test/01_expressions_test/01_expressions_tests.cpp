39f678501c756abc6ba9614a021bc7f78212a048
9419311 <felipe.paz@g.austincc.edu>
2020-01-29 18:46:41 -0600

    Expresions 01

--- test/homework_test/01_expressions_test/01_expressions_tests.cpp
+++ test/homework_test/01_expressions_test/01_expressions_tests.cpp
@@ -12,3 +12,9 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("Verify multiply_numbers fuction")
+{
+	REQUIRE(multiply_numbers(10, 5) == 50);
+	REQUIRE(multiply_numbers(13, 12) != 2);
+
+}
