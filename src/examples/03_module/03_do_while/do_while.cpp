f7a498475851a32f664abaa79a0ef6e1cd228781
9419311 <felipe.paz@g.austincc.edu>
2020-02-10 19:12:48 -0600

    Homework 2 and examples

--- src/examples/03_module/03_do_while/do_while.cpp
+++ src/examples/03_module/03_do_while/do_while.cpp
@@ -8,6 +8,15 @@ using std::cin;
 //Write code for void function prompt_user to loop until
 //user opts not to continue.  
 
+void prompt_user()
+{
+	int choice;
+	do
+	{
+		cout << "Continue?...";
+		cin >> choice;
+	} while (choice == 1);
+}
 
 //Write code for function run_menu that prompts  user for a 
 //number from 1 to 4 and displays the option user selected.
--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -1,7 +1,14 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "decisions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify the function calculate_gpa") {
+	REQUIRE(calculate_gpa(12.0 , 45.0 ) == 3.75);
+	REQUIRE(calculate_gpa(120, 390) == 3.25);
+	REQUIRE(calculate_gpa(90, 180) == 2.00);
+	REQUIRE(calculate_gpa(50, 0) == -1);
+}
