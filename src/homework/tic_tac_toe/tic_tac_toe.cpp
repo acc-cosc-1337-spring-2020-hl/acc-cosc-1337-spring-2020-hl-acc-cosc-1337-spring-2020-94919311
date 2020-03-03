a2d0dbcd87ad4ba8c115d12ace0b98ca0957a084
Felipe Andre Paz Schaack <felipeps2020@hotmail.com>
2020-03-03 09:10:18 -0600

    Some of HW 5

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -4,7 +4,7 @@
 
 void TicTacToe::start_game(string first_player)
 {
-	if (first_player != X && first_player != O)
+	if (first_player != "X" && first_player != "O")
 	{
 		throw Error("Player must be X or O");
 	}
@@ -16,13 +16,26 @@ void TicTacToe::start_game(string first_player)
 
 void TicTacToe::mark_board(int position)
 {
-	while (position != 1, 2, 3, 4, 5, 6, 7, 8, 9)
+	while (position != 1 && 2 && 3 && 4 && 5 && 6 && 7 && 8 && 9)
 	{
 		throw Error("Position must be 1 to 9");
 	}
-	if (player = "")
+	if (player == "")
 	{
 		throw Error("Must start game first");
 	}
+	set_next_player();
 
+}
+
+void TicTacToe::set_next_player()
+{
+	if (player == "X")
+	{
+		player = "O";
+	}
+	else
+	{
+		player = "X";
+	}
 }
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -6,9 +6,9 @@ using std::string;
 class TicTacToe
 {
 public:
-	void mark_board(inty position);
+	void mark_board(int position);
 	void start_game(string first_player);
-	string get_player() const;
+	string get_player() const { return player };
 private:
 	void set_next_player();
 	string player;
